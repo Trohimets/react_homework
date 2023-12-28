6d22a5477084f8e2750ede3cd8f55f1c0d3744db
Константин Трохимец <katrohimets@mail.ru>
2023-12-28 16:34:31 +0300

    add Styled library

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react'
-import Card from '../Card/Card'
+import Card from '../Card/Card.jsx'
 import Spinner from '../Spinner/Spinner.js'
+
+import styles from './main.module.css'
 
 const Main = () => {
     const [goods, setGoods] = useState([]);
@@ -18,6 +20,9 @@ const Main = () => {
     {goods.length===0 && <Spinner></Spinner>}
     <h2>Список товаров</h2>
     <main style={{display:'flex', flexWrap: 'wrap', gap: '5%'}}>
+
+
+        <ul className={styles.list__wrapper} >
         {goods.length > 0 && 
             goods.map(({image, title, desc}, id) => {
                 return (<Card
@@ -28,6 +33,7 @@ const Main = () => {
                 />);
         })
     }
+    </ul>
     </main>
     </>
     
---
+++ src/components/Main/styled.js
@@ -0,0 +1,23 @@
+import styled from 'styled-components'
+
+
+export const CardContainer = styled.ul`
+
+
+    padding: 10px;
+
+    gap: 10px 15px;
+    display: flex;
+
+    border: 1px solid #fff;
+    box-sizing: border-box;
+    width: 20%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin: 10px;
+    border: 1px dotted red;
+    border-radius: 5px;
+
+
+`
