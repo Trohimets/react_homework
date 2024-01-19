6a236425593f1e5b3d525f687bbc370f281c2512
Константин Трохимец <katrohimets@mail.ru>
2024-01-19 15:48:32 +0300

    inicialization

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,21 +1,6 @@
-// import { useEffect, useState } from 'react'
-// import Card from '../Card/Card.jsx'
-// import Spinner from '../Spinner/Spinner.js'
-
-// import styles from './main.module.css'
-
 import { LoginForm } from "../LoginForm/LoginForm";
 
 const Main = () => {
-    // const [goods, setGoods] = useState([]);
-
-    // useEffect(() => {
-    //     fetch('https://658c6840859b3491d3f6081d.mockapi.io/api/v1/item')
-    //     .then((res) => res.json())
-    //     .then((data)=> {
-    //         setGoods(()=> data);
-    //     })
-    // }, []);
 
     return (
     <>
