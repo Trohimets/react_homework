7db7353b49e7c30fbf09f4c2ceb9560c62149635
Трохимец Константин <katrohimets@mail.ru>
2024-01-21 14:17:01 +0300

    create boiler-plate

--- src/components/Aside/Aside.js
+++
@@ -1,7 +0,0 @@
-const Aside = () => {
-    return (
-        <aside>Я сайдбар</aside>
-    );
-};
-
-export default Aside;
--- src/components/Spinner/Spinner.js
+++
@@ -1,8 +0,0 @@
-import Styles from './spinner.module.css'
-
-
-const Spinner = () => {
-    return <span className={Styles['loader']}>Loading</span>
-};
-
-export default Spinner
